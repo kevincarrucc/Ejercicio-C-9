3e5d1a80d19f020b02f65ec07c8a2af1eeb68f2e
carru <carrucciokevin@gmail.com>
2024-06-08 15:11:41 -0300

    Agregar archivos de proyecto.

---
+++ Form1.Designer.vb
@@ -0,0 +1,82 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.Boton = New System.Windows.Forms.Button()
+        Me.LblPromedio = New System.Windows.Forms.Label()
+        Me.NumerosCargados = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(61, 4)
+        '
+        'Boton
+        '
+        Me.Boton.Location = New System.Drawing.Point(238, 22)
+        Me.Boton.Name = "Boton"
+        Me.Boton.Size = New System.Drawing.Size(110, 23)
+        Me.Boton.TabIndex = 2
+        Me.Boton.Text = "Cargar numeros"
+        Me.Boton.UseVisualStyleBackColor = True
+        '
+        'LblPromedio
+        '
+        Me.LblPromedio.AutoSize = True
+        Me.LblPromedio.Location = New System.Drawing.Point(235, 377)
+        Me.LblPromedio.Name = "LblPromedio"
+        Me.LblPromedio.Size = New System.Drawing.Size(39, 13)
+        Me.LblPromedio.TabIndex = 3
+        Me.LblPromedio.Text = "Label1"
+        '
+        'NumerosCargados
+        '
+        Me.NumerosCargados.FormattingEnabled = True
+        Me.NumerosCargados.Location = New System.Drawing.Point(92, 22)
+        Me.NumerosCargados.Name = "NumerosCargados"
+        Me.NumerosCargados.Size = New System.Drawing.Size(120, 368)
+        Me.NumerosCargados.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.NumerosCargados)
+        Me.Controls.Add(Me.LblPromedio)
+        Me.Controls.Add(Me.Boton)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents Boton As Button
+    Friend WithEvents LblPromedio As Label
+    Friend WithEvents NumerosCargados As ListBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,5 @@
+﻿Public Class Form1
+    Private Sub Boton_Click(sender As Object, e As EventArgs) Handles Boton.Click
+
+    End Sub
+End Class
